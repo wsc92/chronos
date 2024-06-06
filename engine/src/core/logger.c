6b0df2074a73540fc8320a2ad606fe879394d9a7
cogent <wscraig92@yahoo.com>
2024-06-06 11:03:16 -0400

    adding win32 support

--- engine/src/core/logger.c
+++ engine/src/core/logger.c
@@ -21,7 +21,8 @@ void log_output(log_level level, const char* message, ...) {
 
     // Technically imposes a 32k character limit on a single log entry, but..
     // DON'T DO THAT!
-    char out_message[32000];
+    const i32 msg_length = 32000;
+    char out_message[msg_length];
     memset(out_message, 0, sizeof(out_message));
 
     // Format original message.
@@ -30,13 +31,13 @@ void log_output(log_level level, const char* message, ...) {
     // which is the type GCC/Clang's va_start expects.
     va_list arg_ptr;
     va_start(arg_ptr, message);
-    vsnprintf(out_message, 32000, message, arg_ptr);
+    vsnprintf(out_message, msg_length, message, arg_ptr);
     va_end(arg_ptr);
 
-    char out_message2[32000];
+    char out_message2[msg_length];
     sprintf(out_message2, "%s%s\n", level_strings[level], out_message);
 
-    // TODO: platform-specific output.
+    // platform-specific output.
     if (is_error) {
         platform_console_write_error(out_message2, level);
     } else {
