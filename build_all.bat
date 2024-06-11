65ddb38984f854208661bd083d66156bf35a2f36
cogent <wscraig92@yahoo.com>
2024-06-11 12:38:20 -0400

    added makefiles

--- build_all.bat
+++ build_all.bat
@@ -4,15 +4,23 @@ REM Build Everything
 ECHO "Building everything..."
 
 
-PUSHD engine
-CALL build.bat
-POPD
-IF %ERRORLEVEL% NEQ 0 (echo Error:%ERRORLEVEL% && exit)
+REM PUSHD engine
+REM CALL build.bat
+REM POPD
+REM IF %ERRORLEVEL% NEQ 0 (echo Error:%ERRORLEVEL% && exit)
+REM
+REM PUSHD testbed
+REM CALL build.bat
+REM POPD
+REM IF %ERRORLEVEL% NEQ 0 (echo Error:%ERRORLEVEL% && exit)
 
-PUSHD testbed
-CALL build.bat
-POPD
-IF %ERRORLEVEL% NEQ 0 (echo Error:%ERRORLEVEL% && exit)
+REM engine
+make -f "Makefile.engine.windows.mak" all
+if %ERRORLEVEL% NEQ 0 (echo Error:%ERRORLEVEL% && exit)
+
+REM testbed
+make -f "Makefile.testbed.windows.mak" all
+if %ERRORLEVEL% NEQ 0 (echo Error:%ERRORLEVEL% && exit)
 
 ECHO "All assemblies built successfully."
 
--- build_all.sh
+++ build_all.sh
@@ -4,18 +4,21 @@ set echo on
 
 echo "Building everything..."
 
-pushd engine
-source build.sh
-popd
+# pushd engine
+# source build.sh
+# popd
+make -f Makefile.engine.linux.mak all
 
 ERRORLEVEL=$?
 if [ $ERRORLEVEL -ne 0 ]; then
 	echo "Error:"$ERRORLEVEL && exit
 fi
 
-pushd testbed
-source build.sh
-popd
+# pushd testbed
+# source build.sh
+# popd
+make -f Makefile.testbed.linux.mak all
+
 ERRORLEVEL=$?
 if [ $ERRORLEVEL -ne 0 ]; then
 	echo "Error:"$ERRORLEVEL && exit
