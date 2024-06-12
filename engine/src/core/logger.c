0d009d41c9eb2573b461dccc4c299f68817c1de5
cogent <wscraig92@yahoo.com>
2024-06-12 14:34:40 -0400

    filesystem built and logging updated to write to file

--- engine/src/core/logger.c
+++ engine/src/core/logger.c
@@ -1,17 +1,30 @@
 #include "logger.h"
 #include "asserts.h"
 #include "../platform/platform.h"
+#include "../platform/filesystem.h"
+#include "cmemory.h"
+#include "cstring.h"
+
 
 // TODO: temporary
-#include <stdio.h>
-#include <string.h>
 #include <stdarg.h>
 
 typedef struct logger_system_state {
-    b8 initialized;
+    file_handle log_file_handled;
 } logger_system_state;
 
 static logger_system_state* state_ptr;
+
+void append_to_log_file(const char* message) {
+    if (state_ptr && state_ptr->log_file_handled.is_valid) {
+        // since the message already contains a newline, just write the bytes directly.
+        u64 length = string_length(message);
+        u64 written = 0;
+        if (!filesystem_write(&state_ptr->log_file_handled, length, message, &written)) {
+            platform_console_write_error("ERROR writing to console log", LOG_LEVEL_ERROR);
+        }
+    }
+}
 
 b8 initialize_logging(u64* memory_requirement, void* state) {
     *memory_requirement = sizeof(logger_system_state);
@@ -20,7 +33,14 @@ b8 initialize_logging(u64* memory_requirement, void* state) {
     }
 
     state_ptr = state;
-    state_ptr->initialized = true;
+
+    // create new/wipe existing log file, then open it.
+    if (!filesystem_open("console.log", FILE_MODE_WRITE, false, &state_ptr->log_file_handled)) {
+        platform_console_write_error("ERROR: Unable to open console.log for writing", LOG_LEVEL_ERROR);
+        return false;
+    }
+
+
 
     // TODO: Remove this later
     CFATAL("A test message: %f", 3.14f);
@@ -39,33 +59,35 @@ void shutdown_logging(void* state) {
 }
 
 void log_output(log_level level, const char* message, ...) {
-    const char* level_strings[6] = {"[FATAL]: ", "[ERROR]: ", "[WARN]: ", "[INFO]: ", "[DEBUG]: ", "[TRACE]: ", };
-    b8 is_error = level < LOG_LEVEL_WARN;
+    // TODO: These string operations are all pretty slow. This needs to be
+    // moved to another thread eventually, along with the file writes, to
+    // avoid slowing things down while the engine is trying to run.
+    const char* level_strings[6] = {"[FATAL]:", "[ERROR]:", "[WARN]:", "[INFO]:", "[DEBUG]:", "[TRACE]:"};
+    b8 is_error = level < 2;
 
-    // Technically imposes a 32k character limit on a single log entry, but..
-    // DON'T DO THAT!
-    const i32 msg_length = 32000;
-    char out_message[msg_length];
-    memset(out_message, 0, sizeof(out_message));
+    // Technically imposes a 32k character limit on a single log entry, but...
+    // DON'T DO THAT
+    char out_message[32000];
+    czero_memory(out_message, sizeof(out_message));
 
     // Format original message.
-    // NOTE: Oddly enough, MS's headers override the GCC/Clang va_list type with a "typedef char* va_list" in some
-    // cases, and as a result throws a strange error here. The workaround for now is to just use __builtin_va_list,
-    // which is the type GCC/Clang's va_start expects.
-    va_list arg_ptr;
+    __builtin_va_list arg_ptr;
     va_start(arg_ptr, message);
-    vsnprintf(out_message, msg_length, message, arg_ptr);
+    string_format_v(out_message, message, arg_ptr);
     va_end(arg_ptr);
 
-    char out_message2[msg_length];
-    sprintf(out_message2, "%s%s\n", level_strings[level], out_message);
+    // Prepend log level to message.
+    string_format(out_message, "%s%s\n", level_strings[level], out_message);
 
-    // platform-specific output.
+    // Print accordingly
     if (is_error) {
-        platform_console_write_error(out_message2, level);
+        platform_console_write_error(out_message, level);
     } else {
-        platform_console_write(out_message2, level);
+        platform_console_write(out_message, level);
     }
+
+    // Queue a copy to be written to the log file.
+    append_to_log_file(out_message);
 }
 
 void report_assertion_failure(const char* expression, const char* message, const char* file, i32 line) {
