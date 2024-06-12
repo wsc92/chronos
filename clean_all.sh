ae11832ad6f654b51ecbc65d0abc972002ea7fd2
cogent <wscraig92@yahoo.com>
2024-06-12 13:12:50 -0400

    clean script for rebuilding

--- clean_all.sh
+++ clean_all.sh
@@ -18,4 +18,11 @@ if [ $ERRORLEVEL -ne 0 ]; then
 	echo "Error:"$ERRORLEVEL && exit
 fi
 
+make -f "Makefile.tests.linux.mak" clean
+
+ERRORLEVEL=$?
+if [ $ERRORLEVEL -ne 0 ]; then
+	echo "Error:"$ERRORLEVEL && exit
+fi
+
 echo "All assemblies cleaned successfully."
