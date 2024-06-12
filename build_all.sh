fa0e11bef031575cd1fcda2ce34f52b5d2facfee
cogent <wscraig92@yahoo.com>
2024-06-12 13:11:25 -0400

    build system updates

--- build_all.sh
+++ build_all.sh
@@ -4,21 +4,19 @@ set echo on
 
 echo "Building everything..."
 
-# pushd engine
-# source build.sh
-# popd
 make -f Makefile.engine.linux.mak all
-
 ERRORLEVEL=$?
 if [ $ERRORLEVEL -ne 0 ]; then
 	echo "Error:"$ERRORLEVEL && exit
 fi
 
-# pushd testbed
-# source build.sh
-# popd
 make -f Makefile.testbed.linux.mak all
+ERRORLEVEL=$?
+if [ $ERRORLEVEL -ne 0 ]; then
+	echo "Error:"$ERRORLEVEL && exit
+fi
 
+make -f Makefile.tests.linux.mak all
 ERRORLEVEL=$?
 if [ $ERRORLEVEL -ne 0 ]; then
 	echo "Error:"$ERRORLEVEL && exit
