922e6864d27ca4e7e61cd654ae25c6684803d259
cogent <wscraig92@yahoo.com>
2024-06-14 16:57:06 -0400

    Global Uniform Buffers

--- engine/src/renderer/vulkan/vulkan_backend.c
+++ engine/src/renderer/vulkan/vulkan_backend.c
@@ -245,17 +245,19 @@ b8 vulkan_renderer_backend_initialize(struct renderer_backend* backend, const ch
     vertex_3d verts[vert_count];
     czero_memory(verts, sizeof(vertex_3d) * vert_count);
 
-    verts[0].position.x = 0.0;
-    verts[0].position.y = -0.5;
+    const f32 f = 10.0f;
 
-    verts[1].position.x = 0.5;
-    verts[1].position.y = 0.5;
+    verts[0].position.x = -0.5 * f;
+    verts[0].position.y = -0.5 * f;
 
-    verts[2].position.x = 0;
-    verts[2].position.y = 0.5;
+    verts[1].position.y = 0.5 * f;
+    verts[1].position.x = 0.5 * f;
 
-    verts[3].position.x = 0.5;
-    verts[3].position.y = -0.5;
+    verts[2].position.x = -0.5 * f;
+    verts[2].position.y = 0.5 * f;
+
+    verts[3].position.x = 0.5 * f;
+    verts[3].position.y = -0.5 * f;
 
     const u32 index_count = 6;
     u32 indices[index_count] = { 0, 1, 2, 0, 3, 1 };
@@ -443,6 +445,21 @@ b8 vulkan_renderer_backend_begin_frame(struct renderer_backend* backend, f32 del
     // Begin the render pass.
     vulkan_renderpass_begin(command_buffer, &context.main_renderpass, context.swapchain.framebuffers[context.image_index].handle);
 
+   return true;
+}
+
+void vulkan_renderer_update_global_state(mat4 projection, mat4 view, vec3 view_position, vec4 ambient_colour, i32 mode) {
+    vulkan_command_buffer* command_buffer = &context.graphics_command_buffers[context.image_index];
+
+    vulkan_object_shader_use(&context, &context.object_shader);
+
+    context.object_shader.global_ubo.projection = projection;
+    context.object_shader.global_ubo.view = view;
+
+    // TODO: other ubo properties
+
+    vulkan_object_shader_update_global_state(&context, &context.object_shader);
+    
     // HACK: temp test code
     vulkan_object_shader_use(&context, &context.object_shader);
 
@@ -457,7 +474,6 @@ b8 vulkan_renderer_backend_begin_frame(struct renderer_backend* backend, f32 del
     vkCmdDrawIndexed(command_buffer->handle, 6, 1, 0, 0, 0);
     // HACK: end temp test code
 
-    return true;
 }
 b8 vulkan_renderer_backend_end_frame(struct renderer_backend* backend, f32 delta_time) {
     
