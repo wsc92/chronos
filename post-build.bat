be998d9f86d391b3e9725b13b377b0e5d68965dd
cogent <wscraig92@yahoo.com>
2024-06-24 13:55:10 -0400

    added build tools

---
+++ post-build.bat
@@ -0,0 +1,17 @@
+@echo off
+
+echo "Compiling shaders..."
+
+PUSHD bin
+tools.exe buildshaders ^
+..\assets\shaders\Builtin.MaterialShader.vert.glsl ^
+..\assets\shaders\Builtin.MaterialShader.frag.glsl ^
+..\assets\shaders\Builtin.UIShader.vert.glsl ^
+..\assets\shaders\Builtin.UIShader.frag.glsl ^
+..\assets\shaders\Builtin.UIShader.vert.glsl ^
+..\assets\shaders\Builtin.UIShader.frag.glsl ^
+IF %ERRORLEVEL% NEQ 0 (echo Error:%ERRORLEVEL% && exit)
+
+POPD
+
+echo "Done."
---
+++ post-build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Compiling shaders..."
+
+pushd bin
+# The tools command to build shaders.
+./tools buildshaders \
+	../assets/shaders/Builtin.MaterialShader.vert.glsl \
+	../assets/shaders/Builtin.MaterialShader.frag.glsl \
+	../assets/shaders/Builtin.UIShader.vert.glsl \
+	../assets/shaders/Builtin.UIShader.frag.glsl \
+	../assets/shaders/Builtin.UIShader.vert.glsl \
+	../assets/shaders/Builtin.UIShader.frag.glsl
+
+ERRORLEVEL=$?
+if [ $ERRORLEVEL -ne 0 ]; then
+	echo "Error:"$ERRORLEVEL && exit
+fi
+
+popd
+
+echo "Done."
+
